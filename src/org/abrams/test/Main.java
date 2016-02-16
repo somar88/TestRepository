dad5a5d31a9c88fd8a7af9d5728ad23a5a391b41
Somar Abraham <somar.abraham88@gmail.com>
2016-02-16 22:26:08 +0100

    1

--- src/org/abrams/test/Main.java
+++ src/org/abrams/test/Main.java
@@ -5,8 +5,11 @@ public class Main {
 	public static void main(String[] args){
 		
 		System.out.println("Hello World");
-		TestClass tc;
-		tc = new TestClass();
+//		TestClass tc;
+//		tc = new TestClass();
+		int x = 0;
+		x += 1;
+		System.out.println(x);
 		
 	}
 	
