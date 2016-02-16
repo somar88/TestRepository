c9c8ea3d37947bf26410def222bc8b3dad9e3b4c
Somar Abraham <somar.abraham88@gmail.com>
2016-02-16 22:20:04 +0100

    hi

--- src/org/abrams/test/Main.java
+++ src/org/abrams/test/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args){
 		
 		System.out.println("Hello World");
-//		TestClass tc = new TestClass();
+		TestClass tc;
+		tc = new TestClass();
 		
 	}
 	
