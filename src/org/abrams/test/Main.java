61af1962356d1ba67c5ed009cce6a77abf22a58a
Somar-PC <Somar-PC@DESKTOP-5I4HS3D.fritz.box>
2016-02-20 17:29:59 +0100

    RESTING MY WORK

--- src/org/abrams/test/Main.java
+++ src/org/abrams/test/Main.java
@@ -3,17 +3,6 @@ package org.abrams.test;
 public class Main {
 
 	public static void main(String[] args){
-		
-		System.out.println("Hello World");
-//		TestClass tc;
-//		tc = new TestClass();
-		int x = 0;
-		x += 1;
-		System.out.println(x);
-
-		
-		System.out.println("Hello World");
-		
 	}
 	
 	
---
+++ src/org/abrams/test/Person.java
@@ -0,0 +1,5 @@
+package org.abrams.test;
+
+public class Person{
+
+}
